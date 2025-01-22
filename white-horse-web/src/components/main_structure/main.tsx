b0818e02cc6951ed29e06741cbb6fe3598cf8376
Vinicius Dias <diasvinicius95@outlook.com>
2025-01-21 23:17:23 -0300

    fix: corrigindo url do perfil

--- white-horse-web/src/components/main_structure/main.tsx
+++ white-horse-web/src/components/main_structure/main.tsx
@@ -5,14 +5,17 @@ export default function Main(){
     return (
       <main className="flexStart">
         <img src="./banner2.jpg" alt="Banner principal da Porsche" className="bannerPrincipal"/>
-        <div className="conteiner_promocoes">
-          {/* <h1 className="titlePromocoes">Promoções do dia:</h1>
 
-          <div className="conteiner_cards">
+        <div className="conteiner_promocoes">
+        
+          <h1 className="titlePromocoes">Promoções do dia:</h1>
+
+          {/* <div className="conteiner_cards">
             <CardProdutos />
             <CardProdutos />
             <CardProdutos />
           </div> */}
+          
         </div>
       </main>
     );
--- white-horse-web/src/utils/front_end_urls/urls.ts
+++ white-horse-web/src/utils/front_end_urls/urls.ts
@@ -1,3 +1,3 @@
 const BASE_PATH = "http://localhost:3000";
 
-export const PROFILE = `${BASE_PATH}/profile`;
+export const PROFILE = `${BASE_PATH}/perfil`;
