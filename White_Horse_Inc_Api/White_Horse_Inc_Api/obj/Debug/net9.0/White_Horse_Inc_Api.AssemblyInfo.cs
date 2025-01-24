b42abdc51d313b5af614cf28a374081e23703886
Vinicius Dias <vinicius.rodrigues@fiskaldigital.com.br>
2025-01-24 18:31:27 -0300

    feat: iniciando desenvolvimento e adicionando referencia ao entity framework core

--- White_Horse_Inc_Api/White_Horse_Inc_Api/obj/Debug/net9.0/White_Horse_Inc_Api.AssemblyInfo.cs
+++ White_Horse_Inc_Api/White_Horse_Inc_Api/obj/Debug/net9.0/White_Horse_Inc_Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("White_Horse_Inc_Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa8ac375f32c7c80d799b83090ae1a8548d095c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3217aac3795a1cd6fe4be40366a0fbe5c052ebb")]
 [assembly: System.Reflection.AssemblyProductAttribute("White_Horse_Inc_Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("White_Horse_Inc_Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
