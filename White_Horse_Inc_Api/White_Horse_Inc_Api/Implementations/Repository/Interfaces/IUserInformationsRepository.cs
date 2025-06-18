3a992c1548f0c685372977ab152a037d62eaa4e5
Vinicius Dias <diasvinicius95@outlook.com>
2025-06-18 00:38:38 -0300

    [refactor]: reorganizando estrutura do projeto para melhor visibilidade geral

--- White_Horse_Inc_Api/White_Horse_Inc_Api/Implementations/Repository/Interfaces/IUserInformationsRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using White_Horse_Inc_Core.Interfaces;
-using White_Horse_Inc_Core.Models;
-
-namespace White_Horse_Inc_Api.Implementations.Repository.Interfaces
-{
-    public interface IUserInformationsRepository : IBaseRepository<UserInformations>
-    {
-    }
-}
--- White_Horse_Inc_Api/White_Horse_Inc_Core/Requests/PagedRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace White_Horse_Inc_Core.Requests
-{
-    public class PagedRequest : BaseRequest
-    {
-        public int PageNumber { get; set; } = Configuration.DefaultPageNumber;
-        public int PageSize { get; set; } = Configuration.DefaultPageSize;
-    }
-}
