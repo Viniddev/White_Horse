0dce46a2a65b3f74a4418b243cdb520701ada2d7
Vinicius Dias <diasvinicius95@outlook.com>
2025-06-19 00:35:30 -0300

    [feat]: adicionando modulo de integrationTests + adicionando coverlet.collector + organizando estilos e corrigindo urls

---
+++ RefactorBack/App.IntegrationTests/Program.cs
@@ -0,0 +1,6 @@
+﻿namespace App.IntegrationTests;
+
+public class Program
+{
+    //todo: Implementar os testes de integracao
+}
--- WebProject/src/utils/backEndUrls/urls.ts
+++ WebProject/src/utils/backEndUrls/urls.ts
@@ -1,7 +1,7 @@
-const BASE_PATH = "https://localhost:7156";
+const BASE_PATH = "https://localhost:7223/api/v1";
 
-export const LOGIN = `${BASE_PATH}/V1/login`;
-export const GET_USER_INFORMATIONS = `${BASE_PATH}/V1/GetUserProfileInformations`;
-export const UPDATE_USER_INFORMATIONS = `${BASE_PATH}/V1/UpdateUser`;
+export const LOGIN = `${BASE_PATH}/login`;
+export const GET_USER_INFORMATIONS = `${BASE_PATH}/GetUserProfileInformations`;
+export const UPDATE_USER_INFORMATIONS = `${BASE_PATH}/update`;
 
 export const VIACEP = (e: string) => `https://viacep.com.br/ws/${e}/json/`;
